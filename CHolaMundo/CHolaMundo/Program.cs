d356c017a382e7bf03465864dbd77832622189e3
TamaraCliment <tamaracliment@gmail.com>
2020-01-13 11:31:27 +0100

    Añadido solicitud edad

--- CHolaMundo/CHolaMundo/Program.cs
+++ CHolaMundo/CHolaMundo/Program.cs
@@ -9,6 +9,10 @@ namespace CHolaMundo
 			Console.Write("Introduce tu nombre: ");
 			string nombre = Console.ReadLine();
 			Console.WriteLine("Hola " +  nombre);
+
+            Console.WriteLine("Dime el primer numero:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Tu edad en 2021 es  " + ++edad);
         }
     }
 }
