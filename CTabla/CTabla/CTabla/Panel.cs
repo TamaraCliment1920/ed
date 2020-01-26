357088c5c5baa3e919695571ef38f69fb70396f4
TamaraCliment <tamaracliment@gmail.com>
2020-01-26 21:36:34 +0100

    Panel fuera de la MainWIndow

--- CTabla/CTabla/CTabla/Panel.cs
+++ CTabla/CTabla/CTabla/Panel.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using Gtk;
-using CTabla;
 
-public partial class MainWindow : Gtk.Window
+namespace CTabla
+{ 
+
+public class Panel
 {
-    //private string jugador = "X";
-    public MainWindow() : base(Gtk.WindowType.Toplevel)
-    {
-        {
-            Build();
-            Panel panel= new Panel(vBox);
 
-            /*Table table = new Table( 3, 3, true);
-            int index = 0;
-            for (int row = 0; row < 3; row++)
-                for (int column = 0; column < 3; column++)
+    public Panel(VBox vBox)
+    {
+
+    
+    Table table = new Table(3, 3, true);
+    int index = 0;
+            for (int row = 0; row< 3; row++)
+                for (int column = 0; column< 3; column++)
                 {
                     index++;
                     Button button = new Button();
-                    table.Attach(button, (uint)column, (uint)column+1, (uint)row, (uint)row + 1);
+                    table.Attach(button, (uint) column, (uint) column+1, (uint) row, (uint)row + 1);
                     button.Label = "Button "+ index.ToString();
                     //button.Label = index.ToString();
                     button.Clicked += delegate
@@ -36,20 +36,10 @@ public partial class MainWindow : Gtk.Window
                               jugador = "X";
 
                       };*/
-               // }*/
+                }
 
-           // vBox.Add(table);
-           // table.ShowAll();
-        }
+            vBox.Add(table);
+            table.ShowAll();
     }
-
-
-
-
-    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-    {
-        Application.Quit();
-        a.RetVal = true;
     }
-
-}
+    }
