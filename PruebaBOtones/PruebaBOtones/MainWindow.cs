70ccf79f988985ebc6f163eed3fdbf40f81f683d
TamaraCliment1920 <tamaracliment@gmail.com>
2020-02-04 10:30:46 +0100

    Primeros pasos con GTK

---
+++ PruebaBOtones/PruebaBOtones/MainWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private Random random = new Random();
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBSaludarClicked(object sender, EventArgs e)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "HOLA "+text.Text);
+        md.Run();
+        md.Destroy();
+    }
+
+    protected void OnBSalirClicked(object sender, EventArgs e)
+    {
+        this.Destroy();
+    }
+
+    protected void OnBAleatorioClicked(object sender, EventArgs e)
+    {
+        int indexAleatorio = random.Next(1, 10);
+        MessageDialog a = new MessageDialog(null, DialogFlags.Modal, MessageType.Question, ButtonsType.Close,"Número Aleatorio:"+ indexAleatorio);
+        a.Run();
+        a.Destroy();
+    }
+}
