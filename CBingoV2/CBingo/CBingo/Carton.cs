688898f3601de95263edeb4843cc0d1381c0654c
TamaraCliment <tamaracliment@gmail.com>
2020-02-12 13:21:26 +0100

    Bingo con Ventana Cartón

---
+++ CBingoV2/CBingo/CBingo/Carton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CBingo;
+using Gtk;
+
+namespace CBingo
+{
+    public partial class Carton : Gtk.Window
+    {
+        public Carton() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            Build();
+            Table table = new Table(3, 9, true);
+            int index = 0;
+            for (int row = 0; row < 3; row++)
+                for (int column = 0; column < 9; column++)
+                {
+                    index++;
+                    Button button = new Button();
+                    table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                    button.Label = index.ToString();
+                    vbox2.Add(table);
+                    table.ShowAll();
+                }
+        }
+    }
+}
---
+++ CBingoV2/CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CBingo;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+
+    Bombo bombo = new Bombo();
+    Panel panel;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        panel = new Panel(vbox1);
+        /*Bombo bombo = new Bombo();*/
+        /*BJugar.Clicked += delegate
+        {
+            int numero = bombo.sacarBola();
+
+        };*/
+    }
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+            a.RetVal = true;
+        }
+
+        protected void OnBJugarClicked(object sender, EventArgs e)
+        {
+            /*Bombo bombo = new Bombo();*/
+            int numero = bombo.sacarBola();
+            panel.Marcar(numero);
+
+        }
+
+    protected void OnBCartonClicked(object sender, EventArgs e)
+    {
+        var carton = new Carton();
+    }
+}
